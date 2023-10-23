f57eb34a95d34fffd035cfe2245efab2c0d10992
JorgeIsaacVilla <jvilla10@outlook.es>
2023-10-22 20:18:45 -0500

    primer commit, creacion de landings con perfiles profecionales.

---
+++ js/arrayExperienciaIngeniero.js
@@ -0,0 +1,180 @@
+const experiencias = document.querySelector("#experiencias");
+
+/*   <div  class="canva-experiencia">
+          <div class="portafolio_izquierda">
+               <img class="imagen_web_portafolio" src="./img/drawable-hdpi/Prueb1.PNG"/>
+               <img class="imagen_web_Codigo" src="./img/drawable-hdpi/Prueb1_Cod.PNG"/>
+          </div>
+
+          <div class="text_portafolio_derecho">
+               <p>Replica de prueba plantilla de Freepek. Esta es una de las primeras plantillas que programé en mis comienzos para aprender los conceptos de HTML, y CSS, acompañado de Git y GitHub. La página presentada a continuación no es responsive. Adjunto link de pagina original: <a class="link_referent" href="https://nicepage.com/es/html-templates/preview/estrategias-comerciales-efectivas-1593500?device=desktop">https://nicepage.com/es/html-templates/preview/estrategias-comerciales</a></p>
+
+               <div class="image_portafolio_boton">
+                    <a href="https://github.com/JorgeIsaacVilla/Prueba1"  style="margin-left: 10%;" class="image_boton1"><img src="./img/Trazado 8131.png" alt=""></a>
+                    <a href="https://jorgeisaacvilla.github.io/Prueba1/"  style="margin-left: 3%;" class="image_boton1"><img src="./img/Trazado 8141.png" alt=""></a>
+               </div>
+          </div>
+     </di>
+*/
+const trabajosRealizados = [];
+
+/*trabajosRealizados.push({
+     imageWebPort:"./img/Exp/Monastery.PNG",
+     imageWebCod:"./img/Exp/Monastery_Cod.PNG",
+     text:"Texto ejemplo que explica el proyecto (Maximo 60 Palabras)",
+     enlaceGit:"https://github.com/JorgeIsaacVilla/Prueba1",
+     enlaceWeb:"https://jorgeisaacvilla.github.io/Prueba1/",
+});*/
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen1.1.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen1.jpg",
+     text:"En conforfresh me desempeñé liderando multiples proyectos en los que implicaban diseñar, ensamblar, y montar equipos de refrigeración industrial, trabajando mano a mano con mi equipo de expertos. En esta etapa, creamos un chiller de dos circuitos de refrigeracion con sistema automatizado utilizando PLCs de la marca CAREL, elaborando los planos eléctricos y gestionando todas las conexiones necesarias. También supervisé personalmente la confección del tablero y coordiné su ensamblaje en el equipo. Posteriormente, cargamos el firmware en el controlador, desarrollamos el software y ajustamos con los valores de set-point y tiempos de operación de cada compresor según los requerimientos del cliente. La programación de este chiller se focalizó en su aplicación especializada en el tratamiento de alimentos.",
+});
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen8.8.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen8.jpg",
+     text:"En MYE Ingeniería desarrollé múltiples proyectos de sistemas de potabilización de agua energizados con paneles solares, dirigidos a poblaciones vulnerables en zonas no interconectadas de Colombia. Durante este proceso, elaboré planos, realicé memorias de cálculo y estudios de presupuesto.",
+});
+
+
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen4.4.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen4.jpg",
+     text:"En la empresa ESCO apoye en el diseño de sistemas fotovoltaicas centralizadas, y en el mantenimiento de las mismas. Descargando eventos de los controladores de las islas, e interpretando la información. Apoyando tambien en la base de datos de las poblaciones beneficiadas con este proyecto. ",
+});
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen2.2.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen2.PNG",
+     text:"Participé en proyectos que implicaban el levantamiento de estaciones de potabilización de agua existentes y la realización de rediseños para la actualización de tecnologías con el fin de mejorar el servicio. Durante este proceso, fui responsable de diseñar los planos, así como de llevar a cabo estudios eléctricos y de automatización.",
+});
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen3.3.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen3.jpg",
+     text:"Fui el encargado de energizar tanto instalaciones existentes como las proyectadas, llevando a cabo los estudios respectivos. Diseñé los planos eléctricos y elaboré las cotizaciones del proyecto. Además, realicé un detallado estudio del sistema de puesta a tierra y seleccioné la subestación apropiada para la estación de bombeo, con el objetivo de restablecer el suministro de agua a los usuarios finales. Durante todo el proceso, supervisé de cerca al equipo técnico para garantizar que cumplieran con mis especificaciones de diseño.",
+});
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen5.5.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen5.jpg",
+     text:"Tuve la responsabilidad de llevar a cabo estudios, diseños y cotizaciones de redes de media tensión, tanto en entornos urbanos como industriales.",
+});
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen6.6.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen6.jpg",
+     text:"Tuve a mi cargo la ejecución de levantamientos de sistemas de bombeo de agua potable y residual. Realicé el rediseño de sistemas preexistentes y la actualización de tecnologías, elaborando planos eléctricos y sistemas de control. Implementé lógica cableada o controladores, según las necesidades específicas de cada cliente.",
+});
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen7.7.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen7.jpg",
+     text:"Mi experiencia abarca el diseño de planos, estudios eléctricos y análisis de presupuestos para proyectos en fase de planificación. Destaco por proponer soluciones innovadoras en sintonía con las últimas tecnologías, y además, establezco cronogramas de trabajo para asegurar el cumplimiento de los objetivos planteados.",
+});
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen9.9.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen9.jpg",
+     text:"Mi experiencia abarca la implementación de diversas tecnologías de múltiples modelos, marcas y protocolos de comunicación. Mi destreza radica en conectar y armonizar estos sistemas de manera eficaz, lo que se traduce en procesos sólidos y confiables respaldados por recursos tecnológicos a la vanguardia, diseñando nuevos sistemas completamente funcionales.",
+});
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen12.12.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen12.jpg",
+     text:"Tengo experiencia en el mantenimiento y control de sistemas de telecontrol para interruptores y reconectadores de media tensión, tanto a nivel de subestaciones como de equipos individuales. Mis actividades incluyen la sustitución de piezas y maquinaria, descarga y análisis de datos de eventos, así como el montaje y puesta en funcionamiento de estos equipos.",
+});
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen15.15.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen15.jpg",
+     text:"Cada proyecto que abordo, es un compromiso que realizo con plena conciencia de que aquellos que no pueden llevarlo a cabo, lo necesitan. La necesidad humana y el bienestar colectivo son el principal motor para realizar cada tarea con excelencia. Como líder de equipo, mi objetivo es alinear siempre nuestras acciones con los objetivo corporativos, las personas y la búsqueda de la excelencia.",
+});
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen11.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen11.11.jpg",
+     text:"Desde el principio, me he dedicado a perfeccionar mis habilidades y destrezas como profesional. Me esfuerzo constantemente por mantenerme actualizado y aprender nuevas habilidades para complementar mis conocimientos, lo que me permite ser más competitivo.",
+});
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp-ingeniero/imagen13.13.jpg",
+     imageWebCod:"./img/Exp-ingeniero/imagen13.jpg",
+     text:"Me apasionan los grandes retos y encuentro una gran satisfacción en ver cómo los ideales y objetivos se convierten en realidad. Disfruto siendo parte de este proceso. ",
+});
+
+
+function renderExperiencia(arr){
+     for(experiencia of arr){
+          const canvaExperiencia = document.createElement("div");
+          canvaExperiencia.classList.add("canva-experiencia");
+
+         const portafolioIzquierda = document.createElement("div");
+         portafolioIzquierda.classList.add("portafolio_izquierda");
+ 
+         /*const cuadradoTramsparente = document.createElement("div");
+         cuadradoTramsparente.classList.add("cuadrado_tramsparente");*/
+ 
+         /*const cuadradoGris = document.createElement("div");
+         cuadradoGris.classList.add("cuadrado_gris");*/
+ 
+         const imagenWebPortafolio = document.createElement("img");
+         imagenWebPortafolio.classList.add("imagen_web_portafolio");
+         imagenWebPortafolio.setAttribute("src", experiencia.imageWebPort);
+ 
+         const imagenWebCode = document.createElement("img");
+         imagenWebCode.classList.add("imagen_web_Codigo");
+         imagenWebCode.setAttribute("src", experiencia.imageWebCod);
+ 
+         const textPortafolioDerecho = document.createElement("div");
+         textPortafolioDerecho.classList.add("text_portafolio_derecho");
+ 
+         const textExperiencia = document.createElement("p");
+         textExperiencia.innerText = experiencia.text;
+ 
+         const imagePortafolioBoton = document.createElement("div");
+         imagePortafolioBoton.classList.add("image_portafolio_boton");
+ 
+         /*const enlacGit = document.createElement("a");
+         enlacGit.classList.add("image_boton1");
+         enlacGit.setAttribute("href", experiencia.enlaceGit);
+ 
+         const enlacWeb = document.createElement("a");
+         enlacWeb.classList.add("image_boton1");
+         enlacWeb.setAttribute("href", experiencia.enlaceWeb);*/
+
+         /*imagenes enlaces (inico) */
+         const imageCat = document.createElement("img");
+         imageCat.setAttribute("src", "./img/Trazado 8131.png");
+         const imageAccet = document.createElement("img");
+         imageAccet.setAttribute("src", "./img/Trazado 8141.png");
+         /*imagenes enlaces (fin) */
+         
+         canvaExperiencia.appendChild(portafolioIzquierda)
+         canvaExperiencia.appendChild(textPortafolioDerecho)
+ 
+        /* portafolioIzquierda.appendChild(cuadradoTramsparente);
+         cuadradoTramsparente.appendChild(cuadradoGris);*/
+         portafolioIzquierda.appendChild(imagenWebPortafolio);
+         portafolioIzquierda.appendChild(imagenWebCode);
+ 
+         textPortafolioDerecho.appendChild(textExperiencia);
+         textPortafolioDerecho.appendChild(imagePortafolioBoton);
+ 
+         /*imagePortafolioBoton.appendChild(enlacGit);
+         imagePortafolioBoton.appendChild(enlacWeb);
+
+         enlacGit.appendChild(imageCat);
+         enlacWeb.appendChild(imageAccet);*/
+ 
+         canvaExperiencia.appendChild(textPortafolioDerecho);
+ 
+         experiencias.appendChild(canvaExperiencia);
+     }
+ }
+ 
+ renderExperiencia(trabajosRealizados);
+ 
---
+++ js/arrayExperienciaWeb.js
@@ -0,0 +1,144 @@
+const experiencias = document.querySelector("#experiencias");
+
+/*   <div  class="canva-experiencia">
+          <div class="portafolio_izquierda">
+               <img class="imagen_web_portafolio" src="./img/drawable-hdpi/Prueb1.PNG"/>
+               <img class="imagen_web_Codigo" src="./img/drawable-hdpi/Prueb1_Cod.PNG"/>
+          </div>
+
+          <div class="text_portafolio_derecho">
+               <p>Replica de prueba plantilla de Freepek. Esta es una de las primeras plantillas que programé en mis comienzos para aprender los conceptos de HTML, y CSS, acompañado de Git y GitHub. La página presentada a continuación no es responsive. Adjunto link de pagina original: <a class="link_referent" href="https://nicepage.com/es/html-templates/preview/estrategias-comerciales-efectivas-1593500?device=desktop">https://nicepage.com/es/html-templates/preview/estrategias-comerciales</a></p>
+
+               <div class="image_portafolio_boton">
+                    <a href="https://github.com/JorgeIsaacVilla/Prueba1"  style="margin-left: 10%;" class="image_boton1"><img src="./img/Trazado 8131.png" alt=""></a>
+                    <a href="https://jorgeisaacvilla.github.io/Prueba1/"  style="margin-left: 3%;" class="image_boton1"><img src="./img/Trazado 8141.png" alt=""></a>
+               </div>
+          </div>
+     </di>
+*/
+const trabajosRealizados = [];
+
+/*trabajosRealizados.push({
+     imageWebPort:"./img/Exp/Monastery.PNG",
+     imageWebCod:"./img/Exp/Monastery_Cod.PNG",
+     text:"Texto ejemplo que explica el proyecto (Maximo 60 Palabras)",
+     enlaceGit:"https://github.com/JorgeIsaacVilla/Prueba1",
+     enlaceWeb:"https://jorgeisaacvilla.github.io/Prueba1/",
+});*/
+
+
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp/Monastery.PNG",
+     imageWebCod:"./img/Exp/Monastery_Cod.PNG",
+     text:"Proyecto de Monastery. Esta página web responsiva cuenta con una experiencia audiovisual, interacciones con 3D en realidad aumentada, disponibilidad de idiomas (inglés y español). Este proyecto fue construido en HTML, CSS, JS ya que al ser una página muy visual y con interacciones puntuales, esto me permitiría tener un mayor control del código.",
+     enlaceGit:"https://www.linkedin.com/company/monastery-couture/",
+     enlaceWeb:"https://www.monasterynft.com/",
+});
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp/MenuRestaurante.PNG",
+     imageWebCod:"./img/Exp/MenuRestaurante_Cod.PNG",
+     text:"App móvil web desarrollado con React.js funciones de Pago con código QR, enlazamiento con redes sociales. Enlazamiento interno con HTML puro, y arreglo de listas para cada menú separado por componentes.",
+     enlaceGit:"https://github.com/JorgeIsaacVilla/menu-restaurante/tree/master/src",
+     enlaceWeb:"https://jorgeisaacvilla.github.io/menu-restaurante/",
+});
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp/Marvel.PNG",
+     imageWebCod:"./img/Exp/Marvel_Cod.PNG",
+     text:"Quiero compartir mi proyecto autodidacta desarrollado con React.Esto fue una oportunidad para poner a prueba y fortalecer mis habilidades en React, especialmente en el consumo de APIs y la maquetación web. Pero también le di un toque personal gamer, incorporando elementos como la integración de gráficos 3D y videojuegos.",
+     enlaceGit:"https://github.com/JorgeIsaacVilla/prueb-tec-dcorp",
+     enlaceWeb:"https://jorgeisaacvilla.github.io/prueb-tec-dcorp/",
+});
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp/todoListReact.PNG",
+     imageWebCod:"./img/Exp/todoListReact_Cod.PNG",
+     text:"En mi proyecto de estudio con PLATZI, creé una lista de tareas interactiva en 3D como Frontend. Implementé componentes dinámicos y una base de datos de almacenamiento en la memoria del dispositivo para mantener las tareas visibles hasta su culminación. Descubre más en mi portafolio web.",
+     enlaceGit:"https://github.com/JorgeIsaacVilla/Todo-list-react",
+     enlaceWeb:"https://jorgeisaacvilla.github.io/Todo-list-react/",
+});
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp/Icomer.PNG",
+     imageWebCod:"./img/Exp/Icomer_Cod.PNG",
+     text:"Te comparto Esta e-commerce Programada en HTML, CSS, JS, este proyecto se desarrollo con el fin de demostrar maquetación web, estilos en Css, y aplicación de Arreglos con JavaScript, y buenas prácticas de desarrollo. Accede a los demos para más detalles",
+     enlaceGit:"https://github.com/JorgeIsaacVilla/icomer_enext",
+     enlaceWeb:"https://jorgeisaacvilla.github.io/icomer_enext/",
+});
+trabajosRealizados.push({
+     imageWebPort:"./img/Exp/Prueb2.PNG",
+     imageWebCod:"./img/Exp/Prueb2_Cod.PNG",
+     text:"Programando “lista de chequeo.” Este desarrollo se realizo con la intención de que puedan observar mi técnica de programación con Angular; procurando mantener un sentido estético, coherente y agradable a la vista. ¡Es importante que la primera impresión siempre impacte!",
+     enlaceGit:"https://github.com/JorgeIsaacVilla/TODO_LIST_ANGULAR",
+     enlaceWeb:"https://jorgeisaacvilla.github.io/TODO_LIST_HTML/",
+});
+
+
+function renderExperiencia(arr){
+     for(experiencia of arr){
+          const canvaExperiencia = document.createElement("div");
+          canvaExperiencia.classList.add("canva-experiencia");
+
+         const portafolioIzquierda = document.createElement("div");
+         portafolioIzquierda.classList.add("portafolio_izquierda");
+ 
+         /*const cuadradoTramsparente = document.createElement("div");
+         cuadradoTramsparente.classList.add("cuadrado_tramsparente");*/
+ 
+         /*const cuadradoGris = document.createElement("div");
+         cuadradoGris.classList.add("cuadrado_gris");*/
+ 
+         const imagenWebPortafolio = document.createElement("img");
+         imagenWebPortafolio.classList.add("imagen_web_portafolio");
+         imagenWebPortafolio.setAttribute("src", experiencia.imageWebPort);
+ 
+         const imagenWebCode = document.createElement("img");
+         imagenWebCode.classList.add("imagen_web_Codigo");
+         imagenWebCode.setAttribute("src", experiencia.imageWebCod);
+ 
+         const textPortafolioDerecho = document.createElement("div");
+         textPortafolioDerecho.classList.add("text_portafolio_derecho");
+ 
+         const textExperiencia = document.createElement("p");
+         textExperiencia.innerText = experiencia.text;
+ 
+         const imagePortafolioBoton = document.createElement("div");
+         imagePortafolioBoton.classList.add("image_portafolio_boton");
+ 
+         const enlacGit = document.createElement("a");
+         enlacGit.classList.add("image_boton1");
+         enlacGit.setAttribute("href", experiencia.enlaceGit);
+ 
+         const enlacWeb = document.createElement("a");
+         enlacWeb.classList.add("image_boton1");
+         enlacWeb.setAttribute("href", experiencia.enlaceWeb);
+
+         /*imagenes enlaces (inico) */
+         const imageCat = document.createElement("img");
+         imageCat.setAttribute("src", "./img/Trazado 8131.png");
+         const imageAccet = document.createElement("img");
+         imageAccet.setAttribute("src", "./img/Trazado 8141.png");
+         /*imagenes enlaces (fin) */
+         
+         canvaExperiencia.appendChild(portafolioIzquierda)
+         canvaExperiencia.appendChild(textPortafolioDerecho)
+ 
+        /* portafolioIzquierda.appendChild(cuadradoTramsparente);
+         cuadradoTramsparente.appendChild(cuadradoGris);*/
+         portafolioIzquierda.appendChild(imagenWebPortafolio);
+         portafolioIzquierda.appendChild(imagenWebCode);
+ 
+         textPortafolioDerecho.appendChild(textExperiencia);
+         textPortafolioDerecho.appendChild(imagePortafolioBoton);
+ 
+         imagePortafolioBoton.appendChild(enlacGit);
+         imagePortafolioBoton.appendChild(enlacWeb);
+
+         enlacGit.appendChild(imageCat);
+         enlacWeb.appendChild(imageAccet);
+ 
+         canvaExperiencia.appendChild(textPortafolioDerecho);
+ 
+         experiencias.appendChild(canvaExperiencia);
+     }
+ }
+ 
+ renderExperiencia(trabajosRealizados);
+ 
